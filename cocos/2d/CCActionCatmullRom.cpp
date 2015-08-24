b804c9657032dae16ceb5e33f0bd9472010d97ef
yoshiyuki.nishimura <yoshiyuki.nishimura@uei.co.jp>
2015-08-24 13:43:56 +0900

    スプライン式を改変BSplineに暫定対応

--- cocos/2d/CCActionCatmullRom.cpp
+++ cocos/2d/CCActionCatmullRom.cpp
@@ -197,24 +197,42 @@ void PointArray::reverseInline()
 // CatmullRom Spline formula:
 Vec2 ccCardinalSplineAt(Vec2 &p0, Vec2 &p1, Vec2 &p2, Vec2 &p3, float tension, float t)
 {
-    float t2 = t * t;
-    float t3 = t2 * t;
-    
-    /*
-     * Formula: s(-ttt + 2tt - t)P1 + s(-ttt + tt)P2 + (2ttt - 3tt + 1)P2 + s(ttt - 2tt + t)P3 + (-2ttt + 3tt)P3 + s(ttt - tt)P4
-     */
-    float s = (1 - tension) / 2;
-	
-    float b1 = s * ((-t3 + (2 * t2)) - t);                      // s(-t3 + 2 t2 - t)P1
-    float b2 = s * (-t3 + t2) + (2 * t3 - 3 * t2 + 1);          // s(-t3 + t2)P2 + (2 t3 - 3 t2 + 1)P2
-    float b3 = s * (t3 - 2 * t2 + t) + (-2 * t3 + 3 * t2);      // s(t3 - 2 t2 + t)P3 + (-2 t3 + 3 t2)P3
-    float b4 = s * (t3 - t2);                                   // s(t3 - t2)P4
-    
-    float x = (p0.x*b1 + p1.x*b2 + p2.x*b3 + p3.x*b4);
-    float y = (p0.y*b1 + p1.y*b2 + p2.y*b3 + p3.y*b4);
-	
-	return Vec2(x,y);
+    if( t<0 ) {
+        //４階Ｂスプライン関数
+        float t2 = t*t;
+        float t3 = t*t*t;
+
+        //４階Ｂスプライン関数
+        float B0 = (  -t3  +3*t2 -3*t +1)/6.0f	;
+        float B1 = ( 3*t3  -6*t2      +4)/6.0f	;
+        float B2 = (-3*t3  +3*t2 +3*t +1)/6.0f	;
+        float B3 = (   t3               )/6.0f	;
+
+        return Vec2(
+            B0 * p0.x + B1 * p1.x + B2 * p2.x + B3 * p3.x,
+            B0 * p0.y + B1 * p1.y + B2 * p2.y + B3 * p3.y
+        );
+    } else {
+        float t2 = t * t;
+        float t3 = t2 * t;
+        
+        /*
+         * Formula: s(-ttt + 2tt - t)P1 + s(-ttt + tt)P2 + (2ttt - 3tt + 1)P2 + s(ttt - 2tt + t)P3 + (-2ttt + 3tt)P3 + s(ttt - tt)P4
+         */
+        float s = (1 - tension) / 2;
+        
+        float b1 = s * ((-t3 + (2 * t2)) - t);                      // s(-t3 + 2 t2 - t)P1
+        float b2 = s * (-t3 + t2) + (2 * t3 - 3 * t2 + 1);          // s(-t3 + t2)P2 + (2 t3 - 3 t2 + 1)P2
+        float b3 = s * (t3 - 2 * t2 + t) + (-2 * t3 + 3 * t2);      // s(t3 - 2 t2 + t)P3 + (-2 t3 + 3 t2)P3
+        float b4 = s * (t3 - t2);                                   // s(t3 - t2)P4
+        
+        float x = (p0.x*b1 + p1.x*b2 + p2.x*b3 + p3.x*b4);
+        float y = (p0.y*b1 + p1.y*b2 + p2.y*b3 + p3.y*b4);
+        
+        return Vec2(x,y);
+    }
 }
+
 
 /* Implementation of CardinalSplineTo
  */
