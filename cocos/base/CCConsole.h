46a9d6ff7df6f9e3d236631654ac12f28bc7ed89
yoshiyuki.nishimura <yoshiyuki.nishimura@uei.co.jp>
2015-10-13 19:22:38 +0900

    コンソールバッファを増やしてみた

--- cocos/base/CCConsole.h
+++ cocos/base/CCConsole.h
@@ -54,7 +54,7 @@ typedef SSIZE_T ssize_t;
 NS_CC_BEGIN
 
 /// The max length of CCLog message.
-static const int MAX_LOG_LENGTH = 16*1024;
+static const int MAX_LOG_LENGTH = 160*1024;
 
 /**
  @brief Output Debug message.
